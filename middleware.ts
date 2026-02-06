ba2c648f50a0ac270571fc92739dc733cece09fe
Huynh Nhan <hoaidev@Mac.lan>
2026-02-06 18:02:02 +0700

    Redirect / to /login

--- middleware.ts
+++ middleware.ts
@@ -14,12 +14,7 @@ export function middleware(req: NextRequest) {
 
   return NextResponse.next()
 
-  if (req.nextUrl.pathname === "/") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
-  }
-  return NextResponse.next();
+  
 }
 
 export const config = {
